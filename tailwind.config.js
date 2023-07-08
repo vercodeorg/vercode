a870d1798349551cd4b3a085c2224c8935acfd35
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-08 13:27:03 -0300

    style: added colors variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'dark-gray': '#1A1A1A',
+        'strong-gray': '#31353A',
+        'medium-gray': '#D9D9D9',
+        'light-gray': '#6A6A6A',
+        'gray-background': '#F5F5F7',
+        'dark-purple': '#3F0065',
+        'light-purple': '#B911F3'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
