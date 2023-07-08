21ae0f57c7da9eccef178d33115d3a35214360b1
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-08 13:43:28 -0300

    style: changed colors to variables

--- app/components/ProgressBar/index.tsx
+++ app/components/ProgressBar/index.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image"
 import Subtract from "../../assets/icons/Subtract.svg"
 
-
 const ProgressBar = () => {
     return (
         <div>
             <span className="font-medium level mb-1 px-1">24.000/35.000 Exp</span>
-            <div className="grid bg-[#D9D9D9] w-full lg:h-4 2xl:h-6 rounded-xl">
-                <div className="bg-gradient-to-r from-[#3F0065] to-[#B911F3] relative w-2/3 lg:h-4 2xl:h-6 rounded-xl"></div>
+            <div className="grid bg-medium-gray w-full lg:h-4 2xl:h-6 rounded-xl">
+                <div className="bg-gradient-to-r from-dark-purple to-light-purple relative w-2/3 lg:h-4 2xl:h-6 rounded-xl"></div>
                 <div className="lg:w-6 absolute justify-self-end self-center translate-x-1">
                     <Image src={Subtract} alt="icon" />
                 </div>
