07cc2cdb92877adbdaf3dbffa5d0e989c9f49ee0
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-08 13:44:54 -0300

    style: added real user image

--- app/components/UserInfoCard/index.tsx
+++ app/components/UserInfoCard/index.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image"
+import userPic from "../../../public/eu3.jpeg"
 
 const UserInfoCard = () => {
     return (
         <div className="2xl:w-64 lg:w-64 lg:h-[365px] 2xl:h-96 rounded-3xl pt-10 pb-20 bg-[#F5F5F7] flex flex-col justify-between items-center absolute top-14 lg:mt-16 2xl:mt-24">
-            <div className="lg:w-28 lg:h-28 2xl:w-32 2xl:h-32 rounded-full active mb-4">
-                {/*<Image src={} alt="user-photo" />*/}
+            <div className="lg:w-28 lg:h-28 2xl:w-32 2xl:h-32 mb-4">
+                <Image src={userPic} alt="user-photo" className="rounded-full" />
             </div>
             <div className="flex flex-col items-center">
                 <h2 className="font-semibold text-[#1A1A1A] xl:text-2xl">Pedro Henrique</h2>
