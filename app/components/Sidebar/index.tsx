7e5816df735bf02f20d94a2e5d387d4c6bd55566
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-08 13:45:25 -0300

    style: changed all svg to heroicons components

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -1,13 +1,13 @@
 'use client'
-import userIcon from "../../assets/icons/user_icon.svg"
-import graphIcon from "../../assets/icons/graph_icon.svg"
-import projectsIcon from "../../assets/icons/projects_icon.svg"
-import stagesIcon from "../../assets/icons/stages_icon.svg"
-import socialIcon from "../../assets/icons/social_icon.svg"
-import logoIcon from "../../assets/img/logo icon.png"
 import Image from "next/image"
 import Link from "next/link"
+import logoIcon from "../../../public/logo icon.png"
 import { usePathname } from "next/navigation"
+import { UserIcon } from "@heroicons/react/24/outline"
+import { CubeTransparentIcon } from "@heroicons/react/24/outline"
+import { FolderOpenIcon } from "@heroicons/react/24/outline"
+import { RectangleStackIcon } from "@heroicons/react/24/outline"
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline"
 
 export const SideBar = () => {
 
@@ -20,23 +20,25 @@ export const SideBar = () => {
                         <Image src={logoIcon} alt="triangle-logo" />
                     </Link>
                 </div>
-                <ul className="flex flex-col gap-8 nav-icons align-middle">
+                <ul className="flex flex-col gap-8 nav-icons">
                     <Link href={"/user"}>
-                        <li className={pathname == "/user" ? "active": ""}>
-                            <Image src={userIcon} alt="user-icon" />
+                        <li className={pathname == "/user" ? "active" : "w-full"}>
+                            <UserIcon className="text-white w-7"/>
                         </li>
                     </Link>
                     <li>
-                        <Image src={graphIcon} alt="graph-icon" />
+                        <CubeTransparentIcon className="text-white w-7" />
                     </li>
                     <li>
-                        <Image src={projectsIcon} alt="projects-icon" />
+                        <FolderOpenIcon className="text-white w-7"/>
                     </li>
+                    <Link href={"/levels"}>
+                        <li className={pathname == "/levels" ? "active" : ""}>
+                            <RectangleStackIcon className="text-white w-7"/>
+                        </li>
+                    </Link>
                     <li>
-                        <Image src={stagesIcon} alt="stages-icon" />
-                    </li>
-                    <li>
-                        <Image src={socialIcon} alt="social-icon" />
+                        <ChatBubbleLeftRightIcon className="text-white w-7" />
                     </li>
                 </ul>
             </nav>
