0a035631f56e68f7cf5351ffc7ed303307871fb9
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-11 01:27:10 -0300

    feat: new ProjectProgressBar component

---
+++ app/components/ProjectProgressBar/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Coin from "../../assets/icons/Moeda.svg"
+const ProjectProgressBar = () => {
+    return (
+        <div>
+            <div className="flex justify-between items-center">
+                <span className="font-normal lg:text-xs 2xl:text-sm text-white mb-1">5/5</span>
+                <div className="flex items-center">
+                    <span className="font-semibold text-xs text-[#F0B40D]">650</span>
+                    <div className="w-7">
+                        <Image src={Coin} alt="coin" />
+                    </div>
+                </div>
+            </div>
+            <div className="grid bg-medium-gray w-full h-4 rounded-xl">
+                <div className="bg-gradient-to-r from-dark-purple to-light-purple relative w-3/3 h-4 rounded-xl"></div>
+            </div>
+            <span className="font-normal lg:text-xs 2xl:text-sm text-white mb-1">5.000/5.000 Exp</span>
+        </div>
+    )
+}
+export default ProjectProgressBar
