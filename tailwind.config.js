17458d37eb676bc34655cc5baf42ab07cf3a3c05
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-11 01:23:02 -0300

    style: create difficulty color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,10 @@ module.exports = {
         'light-gray': '#6A6A6A',
         'gray-background': '#F5F5F7',
         'dark-purple': '#3F0065',
-        'light-purple': '#B911F3'
+        'light-purple': '#B911F3',
+        'level-easy': '#34B949',
+        'level-medium': '#D7CF15',
+        'level-hard': '#FF3124'
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
