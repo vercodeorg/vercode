bb5188c0cbdead0e5a2ad581568f373f7f1da9f5
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-12 13:08:34 -0300

    feat: new Input component

---
+++ app/components/Input/index.tsx
@@ -0,0 +1,12 @@
+
+type TypeProps = {
+    type: string,
+    placeholder: string
+}
+
+const Input = ({type, placeholder}: TypeProps) => {
+    return (
+        <input type={type} className="border border-light-gray hover:ring-2 hover:ring-medium-gray outline-none w-80 h-12 rounded-lg px-4" placeholder={placeholder} />
+    )
+}
+export default Input
