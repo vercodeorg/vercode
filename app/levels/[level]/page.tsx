e80d64c9897f8d8207165c6598495ab3fc759ce8
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-19 09:55:44 -0300

    feat: get name by params on level page

--- app/levels/[level]/page.tsx
+++ app/levels/[level]/page.tsx
@@ -1,17 +1,13 @@
-"use client"
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
 import Badge from "../../assets/icons/Badge.png"
 import SimpleProgressBar from '@/app/components/SimpleProgressBar';
 import LevelProject from '@/app/components/LevelProject';
 
-const Level = () => {
-    const pathname = usePathname();
-    const name = pathname.replace("/levels/", "")
+const Level = ({params}: {params: {level: string}}) => {
     return (
         <div className="w-full bg-cleaner-gray px-28 py-16">
             <div className='flex items-center gap-6 lg:mb-6 2xl:mb-10'>
-                <h1 className="font-semibold lg:text-4xl 2xl:text-5xl tracking-tighter text-dark-gray">{name}</h1>
+                <h1 className="font-semibold lg:text-4xl 2xl:text-5xl tracking-tighter text-dark-gray">{params.level}</h1>
                 <div className='lg:w-16 2xl:w-20'>
                     <Image src={Badge} alt='badge-icon'/>
                 </div>
