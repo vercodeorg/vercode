caa96bceacd7437df6f82ea912487e0ae0d3a6ba
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-21 12:16:20 -0300

    feat: added userId props to UserInfoCard component

--- app/components/UserInfoCard/index.tsx
+++ app/components/UserInfoCard/index.tsx
@@ -2,16 +2,16 @@ import Image from "next/image"
 import userPic from "../../../public/eu3.jpeg"
 import Coin from "../../assets/icons/Moeda.svg"
 
-const UserInfoCard = () => {
+const UserInfoCard = (props: {userId: number}) => {
     return (
-        <div className="2xl:w-64 lg:w-64 lg:h-[365px] 2xl:h-96 rounded-3xl pt-10 pb-20 bg-[#F5F5F7] flex flex-col justify-between items-center absolute top-14 lg:mt-16 2xl:mt-24">
+        <div className="2xl:w-64 lg:w-64  2xl:h-[396px] rounded-3xl pt-10 pb-20 bg-[#F5F5F7] flex flex-col justify-between items-center absolute top-14 lg:mt-16 2xl:mt-24">
             <div className="lg:w-28 lg:h-28 2xl:w-32 2xl:h-32 mb-4">
                 <Image src={userPic} alt="user-photo" className="rounded-full" />
             </div>
             <h2 className="font-semibold text-[#1A1A1A] xl:text-2xl">Pedro Henrique</h2>
             <div className="flex flex-col items-center gap-1">
                 <span className="font-semibold text-medium-gray lg:text-lg 2xl:text-xl">
-                    #472dsfiu
+                    {props.userId}
                 </span>
                 <div className="flex gap-2 items-center">
                     <span className="font-semibold text-amber-500 text-sm">4653</span>
