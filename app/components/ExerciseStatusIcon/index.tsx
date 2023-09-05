396e26e4a6929d6093ebb24dde5ab029d09447a9
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-05 20:30:38 -0300

    enchant: removed not_started status icon

--- app/components/ExerciseStatusIcon/index.tsx
+++ app/components/ExerciseStatusIcon/index.tsx
@@ -1,12 +1,11 @@
 import { EXERCISE_STATUS } from "@/app/types/exerciseStatus"
 import Image from "next/image"
-import notStartedIcon from "../../assets/icons/question-mark.png"
 import completedIcon from "../../assets/icons/check-mark.png"
 import failedIcon from "../../assets/icons/cross-mark.png"
 
 const StatusIcon = ({statusName}: {statusName : EXERCISE_STATUS}) => {
     if(statusName == EXERCISE_STATUS.NOT_STARTED){
-        return <Image src={notStartedIcon} alt="question-icon" />
+        return <div/>
     } else if(statusName == EXERCISE_STATUS.COMPLETED){
         return <Image src={completedIcon} alt="check-icon" />
     } else {
