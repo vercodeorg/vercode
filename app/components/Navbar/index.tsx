919356e37885a7c30b25e1fbad492dab05a017fa
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-05 20:46:32 -0300

    enchant: add to navbar global store coins

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -3,16 +3,20 @@ import vercodeLogo from "../../assets/img/Logo.png"
 import vcoin from "../../assets/icons/vcoin.svg"
 import userPic from "../../../public/eu3.jpeg"
 import Link from "next/link"
+import { useStudentStore } from "@/app/store/store"
 
 export const Navbar = () => {
+    const coins = useStudentStore(state => state.coins)
+
     return (
         <div className="xl:h-14 w-full flex items-center px-4 justify-between">
             <div className="w-36">
                 <Image src={vercodeLogo} alt="vercode-name-logo" width={120} />
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center gap-3">
+                <span>your coins:</span>
                 <div className="flex mr-8 gap-2 items-center">
-                    <span className="font-semibold text-amber-500 text-sm">4653</span>
+                    <span className="font-semibold text-amber-500 text-sm">{coins}</span>
                     <Image src={vcoin} alt="coin-icon" width={25} />
                 </div>
                 <div className="flex items-center gap-2">
