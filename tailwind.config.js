ca64a9a799151205137667c8dae6185b08d236b4
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-09-25 01:31:19 -0300

    feat: removed unecessary type files content on tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ const {nextui} = require("@nextui-org/react");
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
